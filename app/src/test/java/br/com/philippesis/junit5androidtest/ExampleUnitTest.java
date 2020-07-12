1b8973b347f0f54ed1074f52fe0fe2e9c9b4b767
TonyTandAndroid <tonythompsoncmu@gmail.com>
2020-07-12 16:52:32 -0700

    Add library that uses JUnit5

--- app/src/test/java/br/com/philippesis/junit5androidtest/ExampleUnitTest.java
+++ app/src/test/java/br/com/philippesis/junit5androidtest/ExampleUnitTest.java
@@ -1,12 +1,11 @@
 package br.com.philippesis.junit5androidtest;
 
 
+import br.com.philippesis.junit5androidtest.auxiliarclasses.Calculator;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
-
-import br.com.philippesis.junit5androidtest.auxiliarclasses.Calculator;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,41 +18,19 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  */
 public class ExampleUnitTest {
     @Test
-    @DisplayName("Teste inicial JUnit 5 com Android Java")
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
-
-    @Test
+    @DisplayName("Extra Teste inicial JUnit 5 com Android Java")
     void checkAllOperations() {
 
-        final Calculator calculator = new Calculator(6, 2);
+        final Calculator calculator = new Calculator(10, 5);
 
         assertAll("Operações",
-                () -> assertEquals(8, calculator.add()),
-                () -> assertEquals(4, calculator.subtract()),
-                () -> assertEquals(12, calculator.multiply()),
-                () -> assertEquals(3, calculator.divide()));
+                () -> assertEquals(15, calculator.add()),
+                () -> assertEquals(5, calculator.subtract()),
+                () -> assertEquals(50, calculator.multiply()),
+                () -> assertEquals(2, calculator.divide()));
 
     }
 
-    @Test
-    @DisplayName("Validação de exceção.")
-    void checkException() {
 
-        final Calculator calculator = new Calculator(3, 0);
-
-        Throwable exception = assertThrows(Exception.class, calculator::divide);
-        assertEquals("/ by zero", exception.getMessage());
-
-    }
-
-    // Repete test
-    @RepeatedTest(5)
-    void testExample() { assertEquals(2, 1+1); }
-
-    @Disabled
-    @Test
-    void testIgnoredExample() { assertEquals(2, 1+1); }
 
 }
